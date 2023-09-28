5459550daa04c67577d807fa39f6ad35b3f4a947
teo <matiussw@gmail.com>
2023-09-28 15:37:07 -0500

    Entities

---
+++ PGEA.shared/Entities/AcademicEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PGEA.shared.Entities
+{
+	public class AcademicEvent
+	{
+		public AcademicEvent()
+		{
+		}
+	}
+}
+
---
+++ PGEA.shared/Entities/Attendee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PGEA.shared.Entities
+{
+	public class Attendee
+	{
+		public Attendee()
+		{
+		}
+	}
+}
+
