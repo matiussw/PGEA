d8f0d28c2be0b3e3b2cd30e7dc9da3fcb117e824
teo <matiussw@gmail.com>
2023-09-28 19:52:24 -0500

    Repositories web and shared

--- PGEA.WEB/Program.cs
+++ PGEA.WEB/Program.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PGEA.WEB;
+using PGEA.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7218/") });
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
-
